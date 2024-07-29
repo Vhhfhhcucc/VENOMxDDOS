b5034d73ef03cba2d1056abdb4388068abee48e2
Imran Imran <imranattoli@gmail.com>
2024-07-29 19:23:55 +0530

    Update m.py
    Signed-off-by: Imran Imran <imranattoli@gmail.com>

--- m.py
+++ m.py
@@ -8,10 +8,10 @@ import os
 from keep_alive import keep_alive
 keep_alive()
 # insert your Telegram bot token here
-bot = telebot.TeleBot('TOKEN')
+bot = telebot.TeleBot('7024688692:AAH3YKvNir1nZ7CNz3UVVxU38KeP-Z43QCc')
 
 # Admin user IDs
-admin_id = ["your id"]
+admin_id = ["1585506932"]
 
 # File to store allowed user IDs
 USER_FILE = "users.txt"
